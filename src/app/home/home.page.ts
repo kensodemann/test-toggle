75a37037ba54b1422ccafbd2740b1d1b0e98412b
Ken Sodemann <ken@ionic.io>
2021-03-12 15:47:58 -0600

    stop immediate propagation

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -3,10 +3,16 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  styleUrls: ['home.page.scss']
 })
 export class HomePage {
+  clicks = 0;
 
   constructor() {}
 
+  toggle(evt: Event) {
+    console.log(evt);
+    evt.stopImmediatePropagation();
+    this.clicks++;
+  }
 }
